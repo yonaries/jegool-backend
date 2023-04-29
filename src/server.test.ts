afe7a4d446d6d804af4dcc3ed6f83beb368e5216
yonaries <yonaries.dev@gmail.com>
2023-04-30 01:30:10 +0300

    fix(test): uncomment tests for github action testing

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,8 +1,8 @@
-// import { getPort } from "./server"
+import { getPort } from "./server"
 
-// describe('Getting port ', () => {
-//     test('should be 5000', (done) => {
-//         expect(getPort()).toBe(5000)
-//         done();
-//     })
-// })
+describe('Getting port ', () => {
+    test('should be 5000', (done) => {
+        expect(getPort()).toBe(5000)
+        done();
+    })
+})
