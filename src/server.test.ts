b9a0471eadcb074ffabdd659ba6bb74b69466462
yonaries <yonaries.dev@gmail.com>
2023-04-29 17:11:27 +0300

    setup(node): express, jest and scripts

---
+++ src/server.test.ts
@@ -0,0 +1,8 @@
+// import { getPort } from "./server"
+
+// describe('Getting port ', () => {
+//     test('should be 5000', (done) => {
+//         expect(getPort()).toBe(5000)
+//         done();
+//     })
+// })
