9b0cde877025c85fdd0889c7b7265ca88bf8e109
yonaries <yonaries.dev@gmail.com>
2023-05-02 20:36:55 +0300

    sync with main

--- src/test.server.ts
+++ src/test.server.ts
@@ -2,11 +2,7 @@ import express from "express"
 
 const runTestServer = (port: number, callback: Function) => {
     const app = express()
-<<<<<<< HEAD
     return app.listen(port, callback(app))
-=======
-    app.listen(port, callback(app)).close()
->>>>>>> origin/main
 }
 
 export default runTestServer
