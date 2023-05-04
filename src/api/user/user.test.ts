c590987098eac3dba604f4e420809ea2bdf781f4
yonaries <yonaries.dev@gmail.com>
2023-05-04 15:08:46 +0300

    Add user creation failure test and user retrieval test in /user endpoint tests

--- src/api/user/user.test.ts
+++ src/api/user/user.test.ts
@@ -8,7 +8,7 @@ describe("/user", () => {
     app.close();
   });
 
-  describe("GET /", () => {
+  describe("POST /", () => {
     test("should create a new user", async () => {
       const email = fakeEmail()
       const res = await request(app).post("/user").send({
@@ -19,6 +19,26 @@ describe("/user", () => {
         residence: "Et",
       });
       expect(res.status).toBe(201);
+    });
+  });
+
+  describe("POST /", () => {
+    test("should fail to create a new user with invalid request", async () => {
+      const res = await request(app).post("/user").send({
+        firstName: "test",
+        lastName: "test",
+        displayName: "test",
+        email: "test",
+        residence: "Et",
+      });
+      expect(res.status).toBe(400);
+    });
+  });
+
+  describe("GET /", () => {
+    test("should get all users", async () => {
+      const res = await request(app).get("/user");
+      expect(res.status).toBe(200);
     });
   });
 });
