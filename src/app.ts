fde30ef8ba39eb823e64cba4fd72420089bdbb60
Rewina <zerourewina9@gmail.com>
2023-05-05 22:23:00 +0300

    app and server separated

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+import userRouter from "./api/user/user.routes";
+import pageRouter from "./api/page/page.routes";
+import membershipRouter from "./api/membership/membership.routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use(morgan("combined"));
+
+app.use("/user", userRouter);
+app.use("/page", pageRouter);
+app.use("/membership", membershipRouter);
+app.get("/", (_req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,8 @@
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-
+import app from "./app";
 import { getPort } from "./port";
-import userRouter from "./api/user/user.routes";
-import pageRouter from "./api/page/page.routes";
-import membershipRouter from "./api/membership/membership.routes";
 
-const app = express();
 const port = getPort();
 
-app.use(express.json());
-app.use(cors({ origin: "*" }));
-app.use(morgan("combined"));
-
-app.use("/user", userRouter);
-app.use("/page", pageRouter);
-app.use("/membership", membershipRouter);
-app.get("/", (_req, res) => {
-  res.status(200).send("Hello World!");
-});
-
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-export default server;
