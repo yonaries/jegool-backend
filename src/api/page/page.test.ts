de001f914c8e242e1a38fbb2c6482f344db8e60a
yonaries <yonaries.dev@gmail.com>
2023-05-05 10:55:38 +0300

    Add a test for creating a new page with a valid request and owner ID

--- src/api/page/page.test.ts
+++ src/api/page/page.test.ts
@@ -1,10 +1,40 @@
 import request from 'supertest';
 import app from '../../server';
+import fakeEmail from '../utils/randomEmailGenerator';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
 
 
 describe("/page", () => {
     afterEach(async () => {
         app.close();
+    });
+
+    describe("POST /", () => {
+        test("should create a new page", async () => {
+            const email = fakeEmail()
+            let userId: string = "";
+            const createdUser = await prisma.user.create({
+                data: {
+                    firstName: "pagetest",
+                    lastName: "pagetest",
+                    displayName: "pagetest",
+                    email: fakeEmail(),
+                    residence: "ET",
+                },
+            });
+            if (createdUser) {
+                userId = createdUser.id;
+            }
+
+            const name = (Math.random() + 1).toString(36).substring(7)
+            const res = await request(app).post("/page").send({
+                ownerId: userId,
+                name: name
+            });
+            expect(res.status).toBe(201);
+        }, 10000);
     });
 
     describe("POST /", () => {
