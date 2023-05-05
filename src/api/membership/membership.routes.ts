9d98fdbd391dbd0288ee4c3ef7311a59429c4ea1
Rewina <zerourewina9@gmail.com>
2023-05-05 17:30:33 +0300

    feat:get membership by id feature implemented

--- src/api/membership/membership.routes.ts
+++ src/api/membership/membership.routes.ts
@@ -4,5 +4,6 @@ import MembershipController from "./membership.controller";
 const router = express.Router();
 
 router.post("/", MembershipController.createMembership);
+router.get("/:id", MembershipController.getMembershipById);
 
 export default router;
