30f21019ed1e336aeee5391615ca97f7dfc41717
yonaries <yonaries.dev@gmail.com>
2023-05-05 08:35:55 +0300

    Remove redundant test case for creating a new page with valid request

--- src/api/page/page.test.ts
+++ src/api/page/page.test.ts
@@ -8,16 +8,6 @@ describe("/page", () => {
     });
 
     describe("POST /", () => {
-        test("should create a new page", async () => {
-            const res = await request(app).post("/page").send({
-                ownerId: "clh920wpy000cq3yzcn7ijqce",
-                name: "yonaries",
-            });
-            expect(res.status).toBe(201);
-        });
-    });
-
-    describe("POST /", () => {
         test("should fail to create a new page with invalid request", async () => {
             const res = await request(app).post("/page").send({
             });
