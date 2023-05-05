13b8bc0f2f745c5d435a1065463e0d8934883651
yonaries <yonaries.dev@gmail.com>
2023-05-05 11:50:09 +0300

    Add endpoint for updating a page and its corresponding service function

--- src/api/page/page.routes.ts
+++ src/api/page/page.routes.ts
@@ -4,5 +4,6 @@ import PageController from './page.controller'
 const router = express.Router()
 
 router.post('/', PageController.createPage)
+router.put('/:id', PageController.updatePage)
 
 export default router
