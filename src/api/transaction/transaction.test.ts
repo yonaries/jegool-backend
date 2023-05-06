7d3f69175b89fb8d00956839d1041479fa66d8b5
Rewina <zerourewina9@gmail.com>
2023-05-06 23:59:54 +0300

    feature get transaction implemented

--- src/api/transaction/transaction.test.ts
+++ src/api/transaction/transaction.test.ts
@@ -35,14 +35,8 @@ const incorrectDataMissingProperty = {
 
 describe("/transaction", () => {
   describe("POST /", () => {
-    beforeAll(async () => {
-      await prisma.transaction.deleteMany();
-    }, 20000);
-
     test("should return 409 if transaction already exists", async () => {
-      const testTransaction = await request(app)
-        .post("/transaction")
-        .send(correctData);
+      await request(app).post("/transaction").send(correctData);
 
       const res = await request(app).post("/transaction").send(correctData);
       expect(res.status).toBe(409);
@@ -76,4 +70,41 @@ describe("/transaction", () => {
       expect(res.status).toBe(400);
     });
   });
+
+  describe("GET /:reference", () => {
+    let reference: string = "";
+
+    beforeAll(async () => {
+      const testTransaction = await prisma.transaction.create({
+        data: {
+          reference: "12345678910",
+          provider: "chapa",
+          payer: "payer",
+          amount: 50,
+          currency: "etb",
+          remark: "remark",
+          status: "PENDING",
+          payee: "payee",
+        },
+      });
+
+      reference = testTransaction.reference;
+    }, 20000);
+
+    test("should return 200 when transaction is found", async () => {
+      const res = await request(app).get(`/transaction/${reference}`);
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("transaction");
+    });
+
+    test("should return 404 when transaction is not found", async () => {
+      const res = await request(app).get(`/transaction/123456`);
+      expect(res.status).toBe(404);
+    });
+  });
+
+  afterAll(async () => {
+    await prisma.transaction.deleteMany();
+    await prisma.$disconnect();
+  }, 20000);
 });
