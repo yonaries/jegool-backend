8395dcea87bb1a8c33697e6c5ba67783e4704f8e
Yonatan Dejene <yonaries.dev@gmail.com>
2023-05-08 09:35:40 +0300

    Refactor page services to use PrismaClient and handle errors properly (#124)

--- src/api/page/services/page.services.ts
+++ src/api/page/services/page.services.ts
@@ -1,83 +1,80 @@
-import { Page, PrismaClient } from "@prisma/client"
+import { Page, PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export const createPage = async (page: Page): Promise<{ id: string }> => {
-    try {
-        const createdPage = await prisma.page.create({
-            data: page,
-            select: {
-                id: true,
-            },
-        });
-        return createdPage;
+	try {
+		const createdPage = await prisma.page.create({
+			data: page,
+			select: {
+				id: true,
+			},
+		});
+		return createdPage;
+	} catch (error) {
+		throw error;
+	}
+};
 
-    } catch (error) {
-        throw error;
-    }
-
-}
-
-export const updatePage = async (id: string, page: Page): Promise<{ id: string }> => {
-    try {
-        const updatedPage = await prisma.page.update({
-            where: {
-                id: id,
-            },
-            data: page,
-            select: {
-                id: true,
-            },
-        });
-        return updatedPage;
-
-    } catch (error) {
-        throw error;
-    }
-
-}
+export const updatePage = async (
+	id: string,
+	page: Page,
+): Promise<{ id: string }> => {
+	try {
+		const updatedPage = await prisma.page.update({
+			where: {
+				id: id,
+			},
+			data: page,
+			select: {
+				id: true,
+			},
+		});
+		return updatedPage;
+	} catch (error) {
+		throw error;
+	}
+};
 
 export const deletePage = async (id: string): Promise<{ id: string }> => {
-    try {
-        const deletedPage = await prisma.page.delete({
-            where: {
-                id: id,
-            },
-            select: {
-                id: true,
-            },
-        });
-        return deletedPage;
-
-    } catch (error) {
-        throw error;
-    }
-
-}
+	try {
+		const deletedPage = await prisma.page.delete({
+			where: {
+				id: id,
+			},
+			select: {
+				id: true,
+			},
+		});
+		return deletedPage;
+	} catch (error) {
+		throw error;
+	}
+};
 
 export const getPageById = async (id: string): Promise<Page | null> => {
-    try {
-        const page = await prisma.page.findUnique({
-            where: {
-                id: id,
-            },
-        });
+	try {
+		const page = await prisma.page.findUniqueOrThrow({
+			where: {
+				id: id,
+			},
+		});
 
-        return page;
-    } catch (error) {
-        throw error;
-    }
-}
+		return page;
+	} catch (error) {
+		throw error;
+	}
+};
 
 export const getAllPages = async (): Promise<Page[] | null> => {
-    try {
-        const pages = await prisma.page.findMany({
-            orderBy: {
-                createdAt: "desc",
-            },
-        });
-        return pages;
-    } catch (error) {
-        throw error;
-    }
-}
+	try {
+		const pages = await prisma.page.findMany({
+			orderBy: {
+				createdAt: "desc",
+			},
+		});
+		return pages;
+	} catch (error) {
+		throw error;
+	}
+};
