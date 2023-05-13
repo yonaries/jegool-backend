fd863472836c37d2b165e543be943b9247735735
Rewina <zerourewina9@gmail.com>
2023-05-13 13:38:31 +0300

    communityChat route added to app

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ import projectRouter from "./api/project/project.routes";
 import subscriptionRouter from "./api/subscription/subscription.routes";
 import benefitRouter from "./api/benefit/benefit.routes";
 import privateChatRouter from "./api/chat/privateChat/privateChat.routes";
+import communityChatRouter from "./api/chat/communityChat/communityChat.routes";
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use("/project", projectRouter);
 app.use("/subscription", subscriptionRouter);
 app.use("/benefit", benefitRouter);
 app.use("/privateChat", privateChatRouter);
+app.use("/communityChat", communityChatRouter);
 
 app.get("/", (_req, res) => {
  res.status(200).send("Hello World!");
