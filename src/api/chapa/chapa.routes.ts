533e39634893e8b0d1cc2170404ef9343a069943
yonaries <yonaries.dev@gmail.com>
2023-05-14 12:45:25 +0300

    Update Chapa routes to use POST method for callback endpoint

--- src/api/chapa/chapa.routes.ts
+++ src/api/chapa/chapa.routes.ts
@@ -4,7 +4,7 @@ import { validateInitializeChapa } from "./chapa.validate";
 
 const router = Router();
 router.post("/initialize", validateInitializeChapa, ChapaController.initialize);
-router.get("/callback", ChapaController.callback);
+router.post("/callback", ChapaController.callback);
 router.get("/success", ChapaController.success);
 
 export default router;
