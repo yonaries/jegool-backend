5dc9f7cfa33f962d564102cae442754ebd95c891
Rewina <zerourewina9@gmail.com>
2023-05-20 23:27:44 +0300

    feature update bank account implemented

--- src/api/bankAccount/bankAccount.validate.ts
+++ src/api/bankAccount/bankAccount.validate.ts
@@ -9,12 +9,26 @@ const options = {
 };
 
 const bankAccountSchema = Joi.object({
-    id: Joi.string().required(),
+ id: Joi.string().required(),
  bankCode: Joi.string().required(),
  bankName: Joi.string().required(),
  accountNo: Joi.string().required(),
  pageId: Joi.string().required(),
 });
+
+const bankAccountUpdateSchema = Joi.object({
+ id: Joi.string().required(),
+ bankCode: Joi.string().required(),
+ bankName: Joi.string().required(),
+ accountNo: Joi.string().required(),
+});
+
+export const validateBankAccountUpdate = (bankAccount: BankAccount): { error: ValidationError; value: BankAccount } => {
+ return bankAccountUpdateSchema.validate(bankAccount, options) as unknown as {
+  error: ValidationError;
+  value: BankAccount;
+ };
+};
 
 export const validateBankAccount = (bankAccount: BankAccount): { error: ValidationError; value: BankAccount } => {
  return bankAccountSchema.validate(bankAccount, options) as unknown as {
