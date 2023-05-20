528b01041bb5eb7e9acb754ec3a091e6e26ac0af
Rewina <zerourewina9@gmail.com>
2023-05-20 22:48:35 +0300

    feature delete bank account implemented

--- src/api/bankAccount/bankAccount.routes.ts
+++ src/api/bankAccount/bankAccount.routes.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post("/", BankAccountController.createBankAccount);
 router.get("/:pageId", BankAccountController.getBankAccountByPageId);
+router.delete("/:id", BankAccountController.deleteBankAccountById);
 
 export default router;
