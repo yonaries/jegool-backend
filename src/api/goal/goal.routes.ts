a6706b0370d7fb1bf89bbb9396c1f942229292d0
Rewina <zerourewina9@gmail.com>
2023-05-22 00:48:01 +0300

    feature update goal by id implemented

--- src/api/goal/goal.routes.ts
+++ src/api/goal/goal.routes.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/", GoalController.createGoal);
 router.get("/:id", GoalController.getGoalById);
 router.delete("/:id", GoalController.deleteGoalById);
+router.put("/:id", GoalController.updateGoalById);
 
 export default router;
