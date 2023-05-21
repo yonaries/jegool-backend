c5727b460498a15a224d0345777b886f01852c55
Rewina <zerourewina9@gmail.com>
2023-05-21 11:45:18 +0300

    feature get goal by id implemented

--- src/api/goal/goal.routes.ts
+++ src/api/goal/goal.routes.ts
@@ -4,5 +4,6 @@ import GoalController from "./goal.controller";
 const router = express.Router();
 
 router.post("/", GoalController.createGoal);
+router.get("/:id", GoalController.getGoalById);
 
 export default router;
