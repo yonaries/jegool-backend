17d900a8f21c3f351a532b34c2f6034a191ed837
Rewina <zerourewina9@gmail.com>
2023-05-23 07:37:31 +0300

    feature get donationItem implemented

--- src/api/donationItem/donationItem.routes.ts
+++ src/api/donationItem/donationItem.routes.ts
@@ -4,5 +4,6 @@ import DonationItemController from "./donationItem.controller";
 const router = express.Router();
 
 router.post("/", DonationItemController.createDonationItem);
+router.get("/:id", DonationItemController.getDonationItemById);
 
 export default router;
