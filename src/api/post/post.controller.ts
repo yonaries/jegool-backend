2adfa9f4a874a5ba7dce2bd10f135ae2726a4bab
Rewina <79724392+rewinaz@users.noreply.github.com>
2023-06-25 22:38:28 +0300

    delete post controller changed

--- src/api/post/post.controller.ts
+++ src/api/post/post.controller.ts
@@ -33,25 +33,12 @@ export default class PostController {
 
  static deletePost = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const { pageId, userId } = req.body;
 
   if (id.length === 0 || !id) {
    return res.status(400).json({ error: "Post Id Is Required" });
   }
 
   try {
-   const page = await PageServices.getPageById(pageId);
-   if (page?.ownerId !== userId) return res.status(403).json({ error: "Unauthorized request" });
-
-   //todo: replace this with service
-   const postInPage = await PostController.prisma.post.findFirst({
-    where: {
-     id,
-     pageId: pageId,
-    },
-   });
-   if (!postInPage) return res.status(404).json({ error: "Post not found" });
-
    const deletedPost = await PostServices.deletePost(id);
    return res.status(204).json({ post: deletedPost });
   } catch (error) {
